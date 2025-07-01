8980b1e1096f622714f1cc0e24e1798a30a86674
Shiv goyal <shivgoyal9988@gmail.com>
2025-07-01 06:24:54 +0000

    Select verification method:
    AdSense code snippet
    
    Ads.txt snippet
    
    Met

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Russo+One&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8828674905853137"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased">
         <UserProvider>
